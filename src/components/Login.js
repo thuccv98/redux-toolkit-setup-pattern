596e42d23bd2bfc7525f7e6bcbbc8e97d4d45e3c
thuccv98 <thuccv98@gmail.com>
2021-08-29 21:49:39 +0700

    Setup project

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Name: </p>
+      <p>Age: </p>
+      <p>Email: </p>
+    </div>
+  );
+};
+
+export default Profile;
